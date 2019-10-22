01af973045c0c520268c1c1db16bc43b0bfd5a22
SRUTHI <SRUTHI@DESKTOP-9E4ARQD>
2019-10-22 19:51:07 +0530

    Wrapperclass commit

---
+++ wrapperClasses/src/wrapperClassesTesting/Test.java
@@ -0,0 +1,30 @@
+package wrapperClassesTesting;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String s[]= {"10","20"};
+		 String results=s[0]+s[1];
+		 System.out.println("String array additio:"+results);
+		 int x=Integer.parseInt(s[0]);
+		 int y=Integer.parseInt(s[1]);
+		 int r=x+y;
+		 System.out.println("integer array additio:"+r);
+		 float x1=Float.parseFloat(s[0]);
+		 float y1=Float.parseFloat(s[1]);
+		 float rr=x1+y1;
+		 System.out.println("float array additio:"+rr);
+		 //char c1=Character.parseCar();
+		 
+		 int a = 100;
+			
+			Integer obj1 = a;
+			Integer obj2 = new Integer("200");
+			System.out.println(obj1);
+			System.out.println(obj2);
+			Integer obj3 = new Integer("abcd1234");
+			System.out.println(obj3);*/
+	}
+
+}
